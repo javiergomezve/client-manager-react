c2db008f9f7a8cc83beabc540c4e6cad75edc370
Javier Gomez <javiergomezve@gmail.com>
2019-06-16 21:17:33 -0500

    AppNavbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Toute, Switch } from 'react-router-dom';
+
 import './App.css';
+import AppNavbar from './components/layout/AppNavbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hola</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <AppNavbar />
+        <div className="container">
+          <h1>Hola</h1>
+        </div>
+      </div>
+    </Router>
   );
 }
 
