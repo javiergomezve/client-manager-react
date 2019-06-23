3718d1ccb6ea77d8a764567dd96b29a532f901c2
Javier Gomez <javiergomezve@gmail.com>
2019-06-23 17:27:22 -0500

    [add] Authentication

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import store from './store';
+import { UserIsAuthenticated, UserIsNotAuthenticated } from './helpers/auth'
 import AppNavbar from './components/layout/AppNavbar';
 import Dashboard from './components/layout/Dashboard';
 import AddClient from './components/clients/AddClient';
 import ClientDetails from './components/clients/ClientDetails';
 import EditClient from './components/clients/EditClient';
+import Login from './components/auth/Login';
 import './App.css';
 
 function App() {
@@ -18,10 +20,11 @@ function App() {
           <AppNavbar />
           <div className="container">
             <Switch>
-              <Route exact path="/" component={Dashboard} />
-              <Route exact path="/client/add" component={AddClient} />
-              <Route exact path="/client/edit/:id" component={EditClient} />
-              <Route exact path="/client/:id" component={ClientDetails} />
+              <Route exact path="/" component={UserIsAuthenticated(Dashboard)} />
+              <Route exact path="/client/add" component={UserIsAuthenticated(AddClient)} />
+              <Route exact path="/client/edit/:id" component={UserIsAuthenticated(EditClient)} />
+              <Route exact path="/client/:id" component={UserIsAuthenticated(ClientDetails)} />
+              <Route exact path="/login" component={UserIsNotAuthenticated(Login)} />
             </Switch>
           </div>
         </div>
